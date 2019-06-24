4c589779e8081de700919abff06bfc8c7a5a3346
JonReynolds18 <51479517+JonReynolds18@users.noreply.github.com>
2019-06-24 16:19:40 +0100

    Update testGA.py

--- testGA.py
+++ testGA.py
@@ -1,31 +1,18 @@
 # To test the components of the GA from outside the methods
 import numpy as np
 from completeGA import myGA
-from fitness import my_fitness, beale_function, booth_function
+from fitness import my_fitness
+
+# Import the GA and fitness function, create the required parameters and set the desired GA options
 
 number_of_vars = 10
-population_size = 100
 lower_bounds = np.array([-10,-10,-10,-10,-10,-10,-10,-10,-10,-10])
 upper_bounds = np.array([10,10,10,10,10,10,10,10,10,10])
 
-fitness_info = [my_fitness, additional_data]
+fitness_info = [my_fitness]
 
 outcome = myGA(fitness_info, number_of_vars, lower_bounds, upper_bounds, 
                number_of_generations = 100, population_size= 100, elite_fraction= 0.01,
                crossover_fraction= 0.8, mutation_rate=0.1, low_diversity=2)
-
-#number_of_vars = 2
-#lower_bounds = np.array([-4.5, -4.5])
-#upper_bounds = np.array([4.5, 4.5])
-#
-#outcome = myGA(beale_function, number_of_vars, lower_bounds, upper_bounds, 
-#               number_of_generations = 100, mutation_rate = 0.5, crossover_fraction=0.5)
-
-#number_of_vars = 2
-#lower_bounds = np.array([-10, -10])
-#upper_bounds = np.array([10, 10])
-#
-#outcome = myGA(booth_function, number_of_vars, lower_bounds, upper_bounds, 
-#               number_of_generations = 100, low_diversity=1)
 
 optimal = outcome[1]
